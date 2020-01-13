a8b6106a2c4791dcbe35721a654a81073c8f0a39
ChristianOtieno <cgao273@gmail.com>
2020-01-13 23:54:53 +0300

    A test for an initially valid user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'John Doe', email: 'user@mail.com')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
 end
