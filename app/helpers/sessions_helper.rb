09a97e9475bc9738a2652b7c256c3de2ce471961
ChristianOtieno <cgao273@gmail.com>
2020-01-14 22:26:44 +0300

    [SESSIONS-AND-SIGNIN] - Add remember for a persistent user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,12 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
 
   # Logs out the current user.
   def log_out
