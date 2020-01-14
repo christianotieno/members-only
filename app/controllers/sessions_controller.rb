16ada346343d76c22966503d11c07690ac9572eb
Tommy M0ntana <tommy.montana.harwood@gmail.com>
2020-01-14 21:48:30 +0100

    [Sessions and Sign in] -create users session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,16 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    log_in user
-    remember user
-    render 'new'
+    #user = User.find_by(params[:email])
+    if(user && user.authenticate(params[:password]))
+
+    sign_it(user)
+    remember_to user
+    flash[:success] = "Welcome"
+    else
+      flash.now[:success] = "Hello"
   end
+end
 
   def destroy
     sign_out
