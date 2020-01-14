420d116ea6dcffaaaef0a29510dd8eaad8a96060
ChristianOtieno <cgao273@gmail.com>
2020-01-14 21:50:07 +0300

    [SESSIONS-AND-SIGNIN] - Add Logging in and out from a persistent session.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module SessionsHelper
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # Logs out the current user.
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
