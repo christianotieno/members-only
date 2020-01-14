a0ffe423742c9388ef517876ab98e9a3f058483f
Tommy M0ntana <tommy.montana.harwood@gmail.com>
2020-01-14 22:53:26 +0100

    [Sessions and Sign in] -create method in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,36 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  def sign_in(user)
+    new = User.create_token
+    user.remember = new_token
+    cookies.permanent[:remember_token] = new
+    get_current_user
+  end
+
+    def current_user
+        User.find_by(remember_token: cookies[:remember_token])
+    end
+
+    def get_current_user
+        @current_user ||= current_user
+    end
+
+
+  # def sign_in(user)
+  #   cookies.permanent[:remember] = user.remember
+  # end
+
+  # def curren_user
+  #   @current_user = User
+  # end
+
+  # def current_user
+  #   remember = User.remember_digest(cookies[:remember])
+  #   @current_user ||= User.find_by(cookies[:remember] if cookies[:remember])
+  # end
+
+
+
 end
