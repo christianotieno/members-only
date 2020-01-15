89f4937c0d953c8aa6b7538aff196718fa924667
ChristianOtieno <cgao273@gmail.com>
2020-01-15 12:50:48 +0300

    [SESSIONS-AND-SIGNIN] - Set current user whenever a user signs in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,9 +8,9 @@ module SessionsHelper
 
   # Remembers a user in a persistent session.
   def remember(user)
-    user.remember
+    user.update_attribute(:remember_digest, Digest::SHA1.hexdigest(SecureRandom.urlsafe_base64))
     cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
+    cookies.permanent[:remember_digest] = user.remember_digest
   end
 
   def current_user=(user)
@@ -28,8 +28,12 @@ module SessionsHelper
 
   # Logs out the current user.
   def log_out
-    forget(current_user)
     session.delete(:user_id)
+    cookies.delete(:remember_digest)
     @current_user = nil
+  end
+
+  def logged_in?
+    !current_user.nil?
   end
 end
