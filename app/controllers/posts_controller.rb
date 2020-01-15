919e0a81e473b6a1c1f7d83d4c6648ba03c18ba7
ChristianOtieno <cgao273@gmail.com>
2020-01-15 13:16:14 +0300

    [AUTHENTICATION AND POSTS] - Create a Post model and a Posts controller and a corresponding resources

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class PostsController < ApplicationController
+  def new; end
+
+  def create; end
+
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
   root    'sessions#new'
   get     'sessions/new'
   get     '/login', to: 'sessions#new'
