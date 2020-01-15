7c525d1d3c63a8ffd106659f1d6da4605590c06c
Tommy M0ntana <tommy.montana.harwood@gmail.com>
2020-01-15 16:56:52 +0100

    [Authentication and Posts] -add before method to restrict non-signed users whit and create methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  def new; end
+  before_action :logged_in_user, only: %i[new create]
+
+  def new
+    @post = Post.new
+  end
 
   def create; end
 
   def index; end
+
+  private
+
+  def logged_in_user
+    return if logged_in?
+
+    flash.now[:danger] = 'Please log in to access the requested page'
+    redirect_to login_path
+  end
 end
