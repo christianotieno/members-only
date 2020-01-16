07e69f9eb15dcead0b5b3c79f5c3797f75846752
ChristianOtieno <cgao273@gmail.com>
2020-01-16 22:28:09 +0300

    [AUTHENTICATION AND POSTS] - Create User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'posts#index'
   resources :posts, only: %i[new create index]
   resources :users, only: %i[new create]
-  resources :sessions
   get 'sessions/new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
